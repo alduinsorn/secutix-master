b7c527d06703449da379386257a6cb1dff141141
alduinsorn <alduinsorn@gmail.com>
2023-09-11 18:30:15 +0200

    changement de la récuperation des services et des banques grace a NLTK lib, quasiment fini partie adyen

--- scrapping/app.py
+++ scrapping/app.py
@@ -24,7 +24,6 @@ def setup_driver(headless=True):
 
     return driver
 
-
 def parse_url(driver, url, wait_time=2):
     driver.implicitly_wait(wait_time)
     content = None; soup = None
@@ -52,105 +51,166 @@ def parse_url(driver, url, wait_time=2):
 
     return content, soup
 
+    
+def assign_incident_type(part_title, incident_obj):
+    part_title_lowered = part_title.lower()
+    incident_obj.incident_type = IncidentType.OTHER # by default we set the Incident as OTHER
 
-def find_right_tag_containing_time(elem_desc_all, incident_obj, part_type):
-    for desc in elem_desc_all:
-        if parse_time_in_desc(desc.text, incident_obj, part_type):
+    for i in IncidentType:
+        if i.title.lower() in part_title_lowered:
+            incident_obj.incident_type = i
             return
 
-    print("Error no time in every text part")
+def retrieve_raw_desc(part, incident_obj, part_title):
+    all_elem_desc = part.find_all("p")
+    raw = f"{part_title}("
+    for elem in all_elem_desc:
+        if "https://status.adyen.com" not in elem.text: 
+            raw += elem.text
+    raw += ")\n"
+    incident_obj.raw += raw
+    # print("raw", raw)
 
-def parse_time_in_desc(elem_desc, incident_obj, part_type):
-    time_str = re.search(TIME_REGEX, elem_desc)
-    if not time_str: print("Error time not found inside the description\n", elem_desc, "\n"); return False
-    time_str = time_str.group().replace('.', ':') # sometimes they have miss typed
+def search_assign_datetime(elem_desc_all, elem_date, incident_obj, part_title):
+    for desc in elem_desc_all:
+        time_str = re.search(TIME_REGEX, desc.text)
+        if not time_str: continue # search in the different <p> tags
+        time_str = time_str.group().replace('.', ':').replace(" ", "") # sometimes they have miss typed
 
-    parsed_datetime = datetime.datetime.strptime(incident_obj.card_datetime, INPUT_DATETIME_FORMAT)
-    time_var = datetime.datetime.strptime(time_str, INPUT_TIME_FORMAT)
-    new_datetime = datetime.datetime(
-        year=parsed_datetime.year,
-        month=parsed_datetime.month,
-        day=parsed_datetime.day,
-        hour=time_var.hour,
-        minute=time_var.minute
-    )
+        print("search_assign_datetime -> time found")
 
-    new_datetime_str = new_datetime.strftime(OUTPUT_DATETIME_FORMAT)
+        # Datetime normalization 
+        ''' TODO 
+        TRY CATCH REQUIRED
+        '''
+        parsed_datetime = datetime.datetime.strptime(elem_date, INPUT_DATETIME_FORMAT)
+        time_var = datetime.datetime.strptime(time_str, INPUT_TIME_FORMAT)
+        
+        new_datetime = datetime.datetime(
+            year=parsed_datetime.year,
+            month=parsed_datetime.month,
+            day=parsed_datetime.day,
+            hour=time_var.hour,
+            minute=time_var.minute
+        )
+        new_datetime_str = new_datetime.strftime(OUTPUT_DATETIME_FORMAT)
 
-    match part_type:
-        case PartType.RESOLVED.value:
-            incident_obj.resolved_datetime = new_datetime_str
-        case PartType.IDENTIFIED.value:
-            incident_obj.identified_datetime = new_datetime_str
-        case _:
-            print("Error: Unknown PartType passed to the function - ", part_type)
+        match part_title:
+            case PartType.RESOLVED.title:
+                incident_obj.resolved_datetime = new_datetime_str
+            case PartType.IDENTIFIED.title:
+                incident_obj.identified_datetime = new_datetime_str
+            case _:
+                print("Error: Unknown PartType passed to the function - ", part_title)
+        return True
 
+    print("Error no time in every <p> balise")
 
-def retrieve_error_rate(elem_desc_list, incident_obj):
-    # contains the service, get all the text between the two words. Sometimes the service contains multiple "words"
-    id_through = elem_desc_list.index("through") if "through" in elem_desc_list else -1
-    id_starting = elem_desc_list.index("starting") if "starting" in elem_desc_list else -1
-    
-    if id_through != -1: # if "though" not in the text, it should have "for" and "by"
-        elem_service = ' '.join(e for e in elem_desc_list[id_through+1:id_starting])
+'''TODO finish the code in this part'''
+def search_date_in_desc(elem_desc_all, incident_obj):
+    # search for the date in text -> can vary a lot
+    return False
+
+def get_word_index(text_list, word):
+    return text_list.index(word) if word in text_list else -1
+
+def extract_word(text_list, id_start, id_end):
+    return ' '.join(e for e in text_list[id_start+1:id_end])
+
+def process_classic_case(id, desc_word_list, incident_obj, word):
+    id_word = get_word_index(desc_word_list, word)
+    if id_word >= 0:
+        elem_service = extract_word(desc_word_list, id, id_word)
         incident_obj.service = elem_service
-    else:
-        # contains the service
-        id_for = elem_desc_list.index("for") if "for" in elem_desc_list else -1
-        id_transactions = elem_desc_list.index("transactions") if "transactions" in elem_desc_list else -1
 
-        # contains the bank(s) -> can have multiple bank specified
-        id_by = elem_desc_list.index("by") if "by" in elem_desc_list else -1
-
-        elem_service = ' '.join(e for e in elem_desc_list[id_for+1:id_transactions])
-        elem_bank = elem_desc_list[id_by+1]
-
-        incident_obj.service = elem_service
-        incident_obj.specific_bank = elem_bank  
+def process_for_by_case(id_1, id_2, desc_word_list, incident_obj):
+    id_transactions = get_word_index(desc_word_list, "transactions")
+    if id_transactions >= 0:
+        incident_obj.service = extract_word(desc_word_list, id_2, id_transactions)
     
+    id_account = get_word_index(desc_word_list, "account")
+    id_cardholders = get_word_index(desc_word_list, "cardholders")
+    if id_account >= 0 and id_cardholders >= 0:
+        id_end = id_account if id_account >= 0 else id_cardholders
+        incident_obj.banks.append(extract_word(desc_word_list, id_1, id_end))
+
+def search_service_n_bank(elem_desc_all, incident_obj):
+    keyword_actions = {
+        "through": process_classic_case,
+        "for_by": process_for_by_case,
+        "with": process_classic_case
+    }
+            
+    for i, elem_desc in enumerate(elem_desc_all):
+        desc_word_list = elem_desc.text.split()
+        
+        # cas 1 contient mot clé "through"
+        id_through = get_word_index(desc_word_list, "through")
+        
+        # cas 2 contient mot clé "for" et "by" ce qui fournit le service et la banque
+        id_for = get_word_index(desc_word_list, "for")
+        id_by = get_word_index(elem_desc_all, "by")
+        
+        # cas 3 contient mot clé "with"
+        id_with = get_word_index(elem_desc_all, "with")
+        
+        if id_through != -1 and "through" in keyword_actions:
+            keyword_actions['through'](id_through, desc_word_list, incident_obj, "through")
+        elif id_for != -1 and id_by != -1 and "for_by" in keyword_actions:
+            keyword_actions['for_by'](id_for, id_by, desc_word_list, incident_obj)
+        if id_with != -1 and "with" in keyword_actions:
+            keyword_actions['with'](id_with, desc_word_list, incident_obj, "with")
+        
+        # Add all proper names in the bank arrray -> should do everything -> service and bank
+        proper_names = extract_proper_names(elem_desc.text)
+        for bank in proper_names:
+            if bank.strip() != incident_obj.service:
+                incident_obj.banks.append(bank.strip())
+        incident_obj.banks = list(dict.fromkeys(incident_obj.banks))
+        
+def recover_part_infos(part, part_title, incident_obj):
+    elem_desc_all = part.find_all("p") 
+    if len(elem_desc_all) == 0: # sometimes the Resolved part has no text -> not an error
+        print(f"No <p> in the part: {part_title}\n")
+        return
+
+    
+    # Check if the date is written in the description or take the part time
+    elem_date = search_date_in_desc(elem_desc_all, incident_obj)
+    print("elem_date :", elem_date)
+    if not elem_date:
+        elem_date = part.find("span", {"class": ["ds-text-small ds-color-grey-450 ds-margin-left-12"]}).text
+        elem_date = ' '.join(elem_date.split()[:-1]) # remove the CEST or other
+
+    print("elem_date :", elem_date)
+
+    search_assign_datetime(elem_desc_all, elem_date, incident_obj, part_title)
+    retrieve_raw_desc(part, incident_obj, part_title)
+    
+    match part_title:
+        case PartType.IDENTIFIED.title:
+            search_service_n_bank(elem_desc_all, incident_obj)
+
+
 
 def retrieve_offer_conversion():
     
     pass
 
 
-def parse_desc_info(elem_desc_all, incident_obj):
-    return
-    for elem_desc in elem_desc_all[:1]:        
-        elem_desc_list = elem_desc.split()
 
-        match incident_obj.incident_type.value:
-            case IncidentType.ERROR_RATE.value: # this also take the REFUSAL_RATE and RESPONSE_TIME
-                retrieve_error_rate(elem_desc_list, incident_obj)
-            case IncidentType.OFFER_CONVERSION.value:
-                pass
-            case IncidentType.RESOLVED.value:
-                pass
-            case IncidentType.DEGRADED_PERFORMANCE.value:
-                pass
-            case IncidentType.OTHER.value:
-                pass
-
-    
-
-
-def check_incident_type(part_title, incident_obj):
-    part_title_lowered = part_title.lower()
-    incident_obj.incident_type = IncidentType.OTHER # by default we set the Incident as OTHER
-
-    for i in IncidentType:
-        if i.title.lower() in part_title_lowered:
-            incident_obj.incident_type = i 
-
-
-def retrieve_raw_desc(part, incident_obj, part_type):
-    all_elem_desc = part.find_all("p")
-    raw = f"{part_type.title}("
-    for elem in all_elem_desc:
-        if "https://status.adyen.com" not in elem.text: 
-            raw += elem.text
-    raw += ")\n"
-    incident_obj.raw += raw
+# Main function deciding of what we gonna do with the given part
+def process_incident_details(part, part_title, incident_obj):
+    if incident_obj.incident_type.value in [IncidentType.ERROR_RATE.value, IncidentType.REFUSAL_RATE.value, IncidentType.RESPONSE_TIME.value]:
+        recover_part_infos(part, part_title, incident_obj)            
+    elif incident_obj.incident_type.value == IncidentType.OFFER_CONVERSION.value:
+        pass
+    elif incident_obj.incident_type.value == IncidentType.RESOLVED.value:
+        pass
+    elif incident_obj.incident_type.value == IncidentType.DEGRADED_PERFORMANCE.value:
+        pass
+    elif incident_obj.incident_type.value == IncidentType.OTHER.value:
+        pass
 
 
 def scrap_adyen_history(driver, save=False):
@@ -158,6 +218,8 @@ def scrap_adyen_history(driver, save=False):
 
     months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
     # months = ["september"]
+    months = ["january"]
+    
     years = [2023] # can add more but enough for now
     base_url = "https://status.adyen.com/incident-history"
 
@@ -176,7 +238,9 @@ def scrap_adyen_history(driver, save=False):
             incidents_cards = soup.find_all('div', class_='card')
             print("Number of incidents", len(incidents_cards))
 
-            for id_card, card in enumerate(incidents_cards):
+            for id_card, card in enumerate(incidents_cards[:5]):
+                print(f"\n\nCard id : {id_card}")
+                
                 # get the different parts of the incident, contains the Title, Resolved, Identified parts
                 parts = card.find_all(lambda tag: tag.name == 'span' and tag.get('class') == ['status-item', 'ds-width-full'])
                 
@@ -185,7 +249,8 @@ def scrap_adyen_history(driver, save=False):
                     print("No incident or useless one")
                     break
 
-                incident_obj = Incident()
+                incident_obj = Incident(banks=[])
+                print(incident_obj)
 
                 for id_part, part in enumerate(parts):
                     part_title = part.find("span", {"class": ["ds-margin-left-12", "ds-text", "ds-font-weight-bold", "ds-color-black"]})
@@ -196,44 +261,19 @@ def scrap_adyen_history(driver, save=False):
                     
                     # part_title can contains : ["title of the incident", "resolved", "identified", "updated"]
                     part_title = part_title.text
-                    # print("\npart_title", part_title)
-                    check_incident_type(part_title, incident_obj)
                     
-                    match id_part:
-                        case PartType.TITLE.value: # title of the card, the incident
-                            incident_date = part.find("span", {"class": ["ds-text-small", "ds-color-grey-450"]}).text
+                    if id_part == 0: # always true - # title of the card, the incident
+                        incident_date = part.find("span", {"class": ["ds-text-small", "ds-color-grey-450"]}).text
 
-                            incident_obj.title = part_title
-                            incident_obj.card_datetime = ' '.join(incident_date.split()[:-1]) # remove the CEST or other
-                        
-                        case PartType.IDENTIFIED.value: # all the others (Resolved, Identified, Updated)
-                            elem_desc_all = part.find_all("p") # we get all the <p> balises in the case that the informations aren't in the first <p>
+                        incident_obj.title = part_title
+                        incident_obj.card_datetime = ' '.join(incident_date.split()[:-1]) # remove the CEST or other
+                        assign_incident_type(part_title, incident_obj)
 
-                            if len(elem_desc_all) == 0: # sometimes the Resolved part has no text -> no an error
-                                print(f"No <p> in the part: {part_title} - card_id : {id_card}\n")
-                                continue
-
-                            elem_date = part.find("span", {"class": ["ds-text-small ds-color-grey-450 ds-margin-left-12"]}).text
-                            elem_date = ' '.join(elem_date.split()[:-1]) # remove the CEST or other
-                            
-                            retrieve_raw_desc(part, incident_obj, PartType.IDENTIFIED)
-                            find_right_tag_containing_time(elem_desc_all, incident_obj, PartType.IDENTIFIED.value)
-                            parse_desc_info(elem_desc_all, incident_obj)
-
-                        case PartType.RESOLVED.value:
-                            elem_desc = part.find("p")
-
-                            if not elem_desc:
-                                print(f"No <p> in the part Resolved. \n{part_title} - card_id : {id_card}\n")
-                                continue
-                            
-                            elem_desc = elem_desc.text
-
-                            parse_time_in_desc(elem_desc, incident_obj, PartType.RESOLVED.value)
-                            retrieve_raw_desc(part, incident_obj, PartType.RESOLVED)
-                            
-                        case _: pass # Updated not taken into account
-                
+                        # print("After title :", str(incident_obj))
+                        continue
+                    
+                    process_incident_details(part, part_title, incident_obj)
+                print(incident_obj)
                 incidents_dict[year][month].append(incident_obj)
             
     input("Continue...")
@@ -245,5 +285,13 @@ def scrap_adyen_history(driver, save=False):
             json.dump(incidents_dict, output_file, indent=4, default=Incident._to_dict)
 
 
+
+''' Program init '''
+# setup selenium driver
 my_driver = setup_driver(True)
-scrap_adyen_history(my_driver)
+# Download the NLTK named entity recognition dataset if not already downloaded
+download_nltk_data()
+    
+
+
+scrap_adyen_history(my_driver, True)
--- scrapping/utils.py
+++ scrapping/utils.py
@@ -1,32 +1,49 @@
 from enum import Enum
+import nltk
+from nltk import ne_chunk, pos_tag, word_tokenize
+from nltk.tree import Tree
+
 
 class Incident:
-    def __init__(self, title=None, service=None, identified_datetime=None, resolved_datetime=None, raw="",specific_bank=None, card_datetime=None, incident_type=None):
+    def __init__(self, title=None, service=None, identified_datetime=None, resolved_datetime=None, raw="",banks=[], card_datetime=None, incident_type=None):
 
         self.title = title
         self.service = service
         self.identified_datetime = identified_datetime
         self.resolved_datetime = resolved_datetime
         self.raw = raw
-        self.specific_bank = specific_bank
+        self.banks = banks
         self.card_datetime = card_datetime
         self.incident_type = incident_type
 
     def _to_dict(self):
-        return {
+        incident_dict = {
             "title": self.title,
             "service": self.service,
             "identified_datetime": self.identified_datetime,
             "resolved_datetime": self.resolved_datetime,
-            "specific_bank": self.specific_bank,
-            "incident_type": self.incident_type,
-            "raw": self.raw
+            "banks": self.banks,
+            "raw": self.raw,
+            "card_datetime": self.card_datetime
         }
-
+        if self.incident_type:
+            incident_dict["incident_type"] = {
+                "id" : self.incident_type.value,
+                "title" : self.incident_type.title
+            }
+        return incident_dict
+    
+    def __str__(self):
+        attributes = self._to_dict()
+        json_str = "{\n"
+        for key, value in attributes.items():
+            json_str += f'    "{key}": "{value}",\n'
+        json_str += "}"
+        return json_str
     
 
 
-TIME_REGEX = r'\b(?:[0-1]?[0-9]|2[0-3])[:.][0-5][0-9](?:[A-Za-z]{3-4})?'
+TIME_REGEX = r'\b(?:[0-1]?[0-9]|2[0-3])\s*[:.]\s*[0-5][0-9](?:[A-Za-z]{3-4})?'
 INPUT_DATETIME_FORMAT = '%B %d %Y, %H:%M'
 INPUT_TIME_FORMAT = '%H:%M'
 OUTPUT_DATETIME_FORMAT = '%Y-%m-%d %H:%M'
@@ -46,15 +63,43 @@ class PartType(Enum):
 
 class IncidentType(Enum):
     ERROR_RATE = (1,"Elevated Error Rate")
-    REFUSAL_RATE = (1,"Elevated Refusal Rate")
-    RESPONSE_TIME = (1,"Response Time") # can be "Elevated Response Time" or "Increase Response Time"
-    OFFER_CONVERSION = (2,"Offer Conversion Issues")
-    RESOLVED = (3,"[Resolved]") # when the incident is just a report that the saw a problem but it's already solved
-    DEGRADED_PERFORMANCE = (4,"Degraded performance")
-    OTHER = (5, "")
+    REFUSAL_RATE = (2,"Elevated Refusal Rate")
+    RESPONSE_TIME = (3,"Response Time") # can be "Elevated Response Time" or "Increase Response Time"
+    OFFER_CONVERSION = (4,"Offer Conversion Issues")
+    RESOLVED = (5,"[Resolved]") # when the incident is just a report that the saw a problem but it's already solved
+    DEGRADED_PERFORMANCE = (6,"Degraded performance")
+    OTHER = (7, "Other")
 
     def __new__(cls, id, title):
         obj = object.__new__(cls)
         obj._value_ = id
         obj.title = title
         return obj
+
+def download_nltk_data():
+    try:
+        nltk.data.find('maxent_ne_chunker')
+        nltk.data.find('words')
+        nltk.data.find('punkt')
+        nltk.data.find('averaged_perceptron_tagger')
+    except LookupError:
+        nltk.download('maxent_ne_chunker')
+        nltk.download('words')
+        nltk.download('punkt')
+        nltk.download('averaged_perceptron_tagger')
+
+
+
+def extract_proper_names(text):
+    # Tokenize the words into sentences
+    proper_names = []
+    
+    nltk_results = ne_chunk(pos_tag(word_tokenize(text[1:-1])))
+    for nltk_res in nltk_results:
+        if type(nltk_res) == Tree:
+            name = ''
+            for nltk_res_leaf in nltk_res.leaves():
+                name += nltk_res_leaf[0] + ' '
+            proper_names.append(name)
+    
+    return proper_names
